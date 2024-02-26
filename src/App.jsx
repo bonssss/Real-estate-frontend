c81886a68d206116704e32229e5cd7b6466ba994
bonsa <bons6710hos@gmail.com>
2024-02-26 14:20:18 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import './app.css';
 import Company from "./components/company/Company";
 import Property from "./components/Hero/Property/Property";
+import Value from "./components/Value/Value";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
 
   <Company/>
   <Property/>
+  <Value/>
 
  
  </div>
--- src/components/Hero/Property/Property.jsx
+++ src/components/Hero/Property/Property.jsx
@@ -4,17 +4,20 @@ import data from '../../../utils/slider.json';
 import 'swiper/css';
 import {Swiper,SwiperSlide,useSwiper} from 'swiper/react';
 import { color } from 'framer-motion';
+import { SliderSetting } from '../../../utils/common';
 
 export default function Property() {
   return (
     <section className="property-wrapper">
         <div className="paddings innerWidth property-container">
-            <div className=" flexColStart property-header">
+            <div className="property-header flexColStart">
                 <span className='orangeText'>Best Choice</span>
                 <span className='primaryText'>Our Popular Properties</span>
             </div>
 
-            <Swiper>
+            <Swiper {...SliderSetting}>
+
+                <SliderButtons/>
                 {
                     data.map((card,i)=>(
                         <SwiperSlide key={i}>
@@ -37,4 +40,14 @@ export default function Property() {
         </div>
     </section>
   )
+}
+
+const SliderButtons =()=>{
+    const swiper = useSwiper();
+    return(
+        <div className="flexCenter property-button">
+            <button onClick={()=>swiper.slidePrev()}>&lt;</button>
+            <button onClick={()=>swiper.slideNext()}>&gt;</button>
+        </div>
+    )
 }
