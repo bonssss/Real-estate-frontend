9747c92542fd475f26e35ce06c889c3f832a2f50
bonsa <bons6710hos@gmail.com>
2024-02-27 11:07:55 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './header.css'
+import {BiMenuAltRight} from 'react-icons/bi';
+import OutsideClickHandler from "react-outside-click-handler";
 
 export default function Header() {
+
+  const [menuOpened, setMenuOpened]= useState(false); 
+
+  const getMenuStyle=(menuOpened)=>{
+    if(document.documentElement.clientWidth <= 800)
+    {
+      return {right: !menuOpened && "-100%"}
+    }
+  }
   return (
     <section className="header-wrapper">
       <div className=" flexCenter paddings innerWidth header-container">
         <img src="./logo.png" alt="logo" width={100} />
-
-        <div className="flexCenter header-menu">
+<OutsideClickHandler 
+onOutsideClick={()=>{
+  setMenuOpened(false)
+}}>
+        <div className="flexCenter header-menu"
+        style={getMenuStyle(menuOpened)}>
         <a href="">Residencies</a>
         <a href="">Our value</a>
         <a href="">Contact us</a>
@@ -15,7 +30,12 @@ export default function Header() {
         <button className=" button"><a href=""> Contact</a></button>
         
         </div>
+        </OutsideClickHandler>
+        <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
+        <BiMenuAltRight size={30}/>
       </div>
+      </div>
+     
     </section>
   );
 }
